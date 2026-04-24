eb2f3543f506598ff44464ad56c99a75f92760e3
guoyanlong <guoyanlong@baidu.com>
2026-04-24 18:20:09 +0800

    Hero 统计卡：训练板块改为训练场地（1中心2分部）

--- data/site.js
+++ data/site.js
@@ -6,6 +6,6 @@ const SITE = {
     { icon: "🏃", num: "3x", label: "每周训练" },
     { icon: "🏅", num: "2000+", label: "活跃跑者" },
     { icon: "💚", num: "0¥", label: "公益免费" },
-    { icon: "📋", num: "6", label: "训练板块" }
+    { icon: "📍", num: "1中心2分部", label: "训练场地" }
   ]
 }
